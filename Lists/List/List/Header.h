f4025df16c0e5bc48d4aeb1a5889a376dfed1680
Łukasz Goleniec <30418440+Flashrow@users.noreply.github.com>
2018-11-27 19:55:22 +0100

    lists continue

--- Lists/List/List/Header.h
+++ Lists/List/List/Header.h
@@ -5,9 +5,15 @@
 typedef int T;
 struct element;
 
+int generateRandom();
 void addToFront(element *&head, T val);
 void printFromStart(element * head);
 void addRandom(element *& head);
 void addSorted(element * & head, T val);
 void addSortedReq(element * & head, T val);
+void printFromStartReq(element * head, std::ostream&);
+void printFromEndReq(element * head, std::ostream&);
+element *findE(element * head, int val);
+element *findEReq(element * & head, int val);
+void deleteElement(element *&head, element *toDelete);
 #endif // !HEADER_H
--- Lists/List/List/Main.cpp
+++ Lists/List/List/Main.cpp
@@ -16,33 +16,43 @@ int main(int argc, char *argv[]) {
 
 	element * head = nullptr;
 
-	//for (int i = 0; i < 10; i++)
-	//	addRandom(head);
-
-	for (auto i : { 1,3,4,6,8,11 })
+	for (auto i : { 2,4,5,11,2,5,73,2 })
 		addToFront(head, i);
 	printFromStart(head);
 	cout << endl;
 
-	addSorted(head, 5);
+	element *foundElement = findE(head, 11);
+
+	cout << foundElement->value << endl;
+	printFromStart(head);
+	cout << endl;
+	deleteElement(head, head);
+
 	printFromStart(head);
 
 	 cin.get();
 	 return 0;
 }
 
-void addToFront(element * &head, int v) {
-	head = new element {v, head};
+void addToFront(element *&head, T val) {
+	head = new element {val, head};
 }
 
-void printFromStart(element * head) {
+void printFromStart(element *head) {
 	while (head) {
 		cout << head->value<<" ";
 		head = head->pNext;
 	}
 }
 
-void addRandom(element * &head) {
+int generateRandom() {
+	default_random_engine engineR;
+	engineR.seed(chrono::system_clock::now().time_since_epoch().count());
+	uniform_int_distribution<int> distribution(1, 100);
+	return(distribution(engineR));
+}
+
+void addRandom(element *&head) {
 	default_random_engine engineR;
 	engineR.seed(chrono::system_clock::now().time_since_epoch().count());
 	uniform_int_distribution<int> distribution(1, 100);
@@ -50,23 +60,82 @@ void addRandom(element * &head) {
 }
 
 void addSorted(element * & head, T val) {
-	element * headBefore = head;
-	if (head)
-		while (headBefore->pNext) {
-			if (headBefore->pNext->value <= val) {
-				headBefore->pNext = new element{ val, headBefore->pNext };
-				break;
+	element * headB = head;
+	if (head) {
+		if (headB->value >= val)
+			head = new element{ val, head };
+		else
+			while (headB->pNext) {
+				if (headB->pNext->value >= val) {
+					headB->pNext = new element{ val, headB->pNext };
+					break;
+				}
+				headB = headB->pNext;
 			}
-			headBefore = headBefore->pNext;
-		}
+		if (!headB->pNext)
+			headB->pNext = new element{ val, nullptr };
+	}
 	else
-		addToFront(head, val);
+		head = new element{ val, head };
 }
 
-void addSortedReq(element * & head, T val) {
+//TODO
+void addSortedReq(element *&head, T val) {
 	if (head) {
-		if(head->value>=val)
 		addSortedReq(head->pNext, val);
+		if (head->value >= val) {
+			element * newElement = new element{val,head->pNext};
+			head->pNext = newElement;
+		}		
 	}
+}
 
+void printFromStartReq(element *head, std::ostream &ss) {
+	if (head) {
+		ss << head->value << " ";
+		printFromStartReq(head->pNext, ss);
+	}
+}
+void printFromEndReq(element *head, std::ostream &ss) {
+	if (head) {
+		printFromStartReq(head->pNext, ss);
+		ss << head->value << " ";
+	}
+}
+
+element *findE(element * head, int val){
+	while (head) {
+		if (head->value == val)
+			return head;
+		else
+			head = head->pNext;
+	}
+	return nullptr;
+}
+
+//TODO
+element *findEReq(element * & head, int val) {
+
+	if (findEReq(head->pNext, val)->value == val)
+		return head;
+	else
+		return nullptr;
+}
+
+void deleteElement(element *&head, element *toDelete) {
+	element * headBefore = head;
+	element * headB = head;
+	if (head == toDelete) {
+		head = headB->pNext;
+		delete(headB);
+	}else
+	while (headB) {
+		if (headB == toDelete) {
+			headBefore->pNext = headB->pNext;
+			delete(headB);
+			break;
+		}
+		headBefore = headB;
+		headB = headB->pNext;
+	}
 }
